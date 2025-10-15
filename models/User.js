5662c494af446fe5e387a2fc79524d60349e9d56
myaumyauverse <satvik.porwal14@gmail.com>
2025-10-15 09:41:09 +0530

    v1

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['student', 'driver', 'operator'], required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
+
